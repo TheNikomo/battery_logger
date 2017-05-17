bcb3cab8f8da832abaa031fbc56c1529005ffd3b
Niko Montonen <montonen.niko@gmail.com>
2017-05-17 07:55:24 +0300

    Add configparser

--- battery.py
+++ battery.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
+import configparser
 import os
 
 if __name__ == "__main__":
+    # Check that we actually have tp_smapi before doing anything
     if os.path.isdir('/sys/devices/platform/smapi'):
         print("tp_smapi sysfs found")
     else:
         quit("tp_smapi sysfs not found - maybe not installed?")
+
+    config = configparser.ConfigParser()
+    config.read("config.ini")
