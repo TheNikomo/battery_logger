249622d32a559f04b5d28fba21dcf6d3194d5e7d
Niko Montonen <montonen.niko@gmail.com>
2017-05-18 16:46:02 +0300

    Remove db references
    If we're just doing dumb CSV-dumping, don't need to worry about db
    Trivial to rework later

--- battery.py
+++ battery.py
@@ -2,6 +2,14 @@
 import configparser
 import os
 
+
+class Battery(object):
+    """docstring for Battery"""
+    def __init__(self, arg):
+        super(Battery, self).__init__()
+        self.arg = arg
+
+
 if __name__ == "__main__":
     # Check that we actually have tp_smapi before doing anything
     if os.path.isdir('/sys/devices/platform/smapi'):
