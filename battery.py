e677fa3fa45bc2760d8dd3174ec614fe79cf5f24
Niko Montonen <montonen.niko@gmail.com>
2017-05-19 11:40:24 +0300

    Construct list of batteries at start of execution
    Create a Battery object for every battery in the system

--- battery.py
+++ battery.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python3
 import configparser
+import glob
 import os
 
 
 class Battery(object):
-    """docstring for Battery"""
-    def __init__(self, arg):
-        super(Battery, self).__init__()
-        self.arg = arg
-
+    """
+    Battery object, stores path to sysfs
+    device and properties about the device
+    NYI: The properties
+    """
+    def __init__(self, path):
+        self.path = path
 
 if __name__ == "__main__":
     # Check that we actually have tp_smapi before doing anything
     if os.path.isdir('/sys/devices/platform/smapi'):
-        print("tp_smapi sysfs found")
+        print('tp_smapi sysfs found')
+        os.chdir('/sys/devices/platform/smapi')
     else:
-        quit("tp_smapi sysfs not found - maybe not installed?")
+        quit('tp_smapi sysfs not found - maybe not installed?')
 
+    # Read the config
     config = configparser.ConfigParser()
-    config.read("config.ini")
+    config.read('config.ini')
+
+    # Get the batteries we have in the system
+    batteries = []
+    for battery_device in glob.glob('BAT*'):
+        batteries.append(Battery(os.path.abspath(battery_device)))
